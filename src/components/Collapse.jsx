d493b336cd8bb59ee330365ae52b8e0edd1b3aa2
Julien2195 <j.lemaitre24@gmail.com>
2023-01-17 19:56:06 +0100

    ajout page logement.js responsive

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -11,34 +11,35 @@ const Collapse = ({ question }) => {
     } else {
       copy.push(i);
     }
-
     setActive(copy);
   };
 
   return (
     <div>
-      <div
-        key={question.id}
-        className={`collapse-about-container ${
-          active.includes(question.id) ? "active" : "closed"
-        }`}
-      >
-        <div className="collapse-about">
-          <div
-            className=" collapse-infos"
-            onClick={() => handleToggle(question.id)}
-          >
-            <h3>{question.title}</h3>
-            <img className={"collapse-container"} src={vector} alt="vector" />
+      {question && (
+        <div
+          key={question.id}
+          className={`collapse-about-container ${
+            active.includes(question.id) ? "active" : "closed"
+          }`}
+        >
+          <div className="collapse-about">
+            <div
+              className=" collapse-infos"
+              onClick={() => handleToggle(question.id)}
+            >
+              <h3>{question.title}</h3>
+              <img className={"collapse-container"} src={vector} alt="vector" />
+            </div>
           </div>
-        </div>
 
-        {active.includes(question.id) ? (
-          <div className="collapse-description">
-            <p>{question.description}</p>
-          </div>
-        ) : null}
-      </div>
+          {active.includes(question.id) ? (
+            <div className="collapse-description">
+              <p>{question.description}</p>
+            </div>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
 import vectorLeft from "../assets/vector-left.png";
 import vectorRight from "../assets/vector-right.png";
-import rate from "../assets/rating.svg";
-import rateEmpty from "../assets/rating-empty.svg";
+import filledStars from "../assets/rating.svg";
+import emptyStars from "../assets/rating-empty.svg";
+import Collapse from "../components/Collapse";
 
 const Logement = () => {
   let params = useParams();
   const [logement, setLogement] = useState(null);
   const [showImg, setShowImg] = useState(0);
-
   useEffect(() => {
     fetch(`/logements.json`)
       .then((response) => response.json())
@@ -29,64 +30,107 @@ const Logement = () => {
   return (
     <div>
       <Header />
-      {logement && (
-        <div className="slidesShow-image">
-          <img
-            onClick={handlePrevious}
-            className="vector-left"
-            src={vectorLeft}
-            alt="fleche gauche"
-          />
-          <img
-            onClick={handleNext}
-            className="vector-right"
-            src={vectorRight}
-            alt="fleche droite"
-          />
-          <img
-            className="slidesShow-image"
-            src={logement.pictures[showImg]}
-            alt={logement.title}
-          />
-          <span className="number">{`${showImg + 1}/${
-            logement.pictures.length
-          }`}</span>
-          <div className="infos-logement">
-            <div>
-              <h2 className="logement-title">{logement.title}</h2>
-              <h3 className="logement-location">{logement.location}</h3>
+
+      <div className="logement-container">
+        {logement && (
+          <div className="slidesShow-image-container">
+            <img
+              onClick={handlePrevious}
+              className="vector-left"
+              src={vectorLeft}
+              alt="fleche gauche"
+            />
+            <img
+              onClick={handleNext}
+              className="vector-right"
+              src={vectorRight}
+              alt="fleche droite"
+            />
+            <img
+              className="slidesShow-image"
+              src={logement.pictures[showImg]}
+              alt={logement.title}
+            />
+            <span className="number">{`${showImg + 1}/${
+              logement.pictures.length
+            }`}</span>
+            <div className="container">
+              <div>
+                <h2 className="logement-title">{logement.title}</h2>
+                <h3 className="logement-location">{logement.location}</h3>
+
+                <div className="tags-container">
+                  {logement.tags.map((element, i) => (
+                    <span key={i} className="tag-name">
+                      {element}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="host-container">
+                <div className="host">
+                  <h4 className="logement-name">{logement.host.name}</h4>
+                  <img
+                    className="logement-picture-host"
+                    src={logement.host.picture}
+                    alt={logement.host.name}
+                  />
+                </div>
+
+                <div className="rating-container">
+                  {typeof logement.rating === "string"
+                    ? Array.from(
+                        { length: parseInt(logement.rating) },
+                        (_, i) => (
+                          <img
+                            key={i}
+                            src={filledStars}
+                            alt="Etoiles pleines"
+                          />
+                        )
+                      )
+                    : null}
+                  {typeof logement.rating === "string"
+                    ? Array.from(
+                        { length: 5 - parseInt(logement.rating) },
+                        (_, i) => (
+                          <img key={i} src={emptyStars} alt="Etoiles vide " />
+                        )
+                      )
+                    : null}
+                </div>
+              </div>
             </div>
-            <div className="host-container">
-              <h4 className="logement-name">{logement.host.name}</h4>
-              <img
-                className="logement-picture-host"
-                src={logement.host.picture}
-                alt={logement.host.name}
-              />
+            <div className="logement-collapse-container">
+              <div className="collapse-about-margin logement">
+                {
+                  <Collapse
+                    question={{
+                      id: logement.id,
+                      title: "Description",
+                      description: logement.description,
+                    }}
+                  />
+                }
+              </div>
+              <div className="collapse-about-margin logement">
+                {
+                  <Collapse
+                    question={{
+                      id: logement.id,
+                      title: "Equipements",
+                      description: logement.equipments.map((element, i) => (
+                        <li key={i}>{element}</li>
+                      )),
+                    }}
+                  />
+                }
+              </div>
             </div>
           </div>
-          <div className="tags-container">
-            {logement.tags.map((element, i) => (
-              <span key={i} className="tag-name">
-                {element}
-              </span>
-            ))}
-          </div>
-          <div className="rating-container">
-            {typeof logement.rating === "string"
-              ? Array.from({ length: parseInt(logement.rating) }, (_, i) => (
-                  <img key={i} src={rate} alt="Etoiles pleines" />
-                ))
-              : null}
-            {typeof logement.rating === "string"
-              ? Array.from(
-                  { length: 5 - parseInt(logement.rating) },
-                  (_, i) => <img key={i} src={rateEmpty} alt="Etoiles vide " />
-                )
-              : null}
-          </div>
-        </div>
-      )}
+        )}
+      </div>
+      <Footer />
     </div>
   );
 };
